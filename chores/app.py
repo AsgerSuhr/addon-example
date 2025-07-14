b5b1398166df1791b01a9e52f7c42e65d87d3d3c
asger <asger31@gmail.com>
2025-07-14 19:03:59 +0200

    cors

--- chores/app.py
+++ chores/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_cors import CORS 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
+CORS(app, origins=['http://localhost:8000', 'https://home.asgerlanghoff.com'])
 
 @app.route("/")
 def hello_world():
     return "Hey, we have Flask in a Docker container!"
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 
 if __name__ == "__main__":
